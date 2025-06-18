6a7726b75be397aeab96b227683864e4b7dab5cd
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-06-18 08:58:22 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Import path module
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: { // Add this resolve section
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/app': path.resolve(__dirname, './src/app'),
+      '@/data': path.resolve(__dirname, './src/data'),
+      '@/domain': path.resolve(__dirname, './src/domain'),
+    },
+  },
 })
